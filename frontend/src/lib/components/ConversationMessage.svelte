6893f9bfeaab3e2b2fb51ad49883f584c5108c4f
Alice Lee <alice.lee1@digital.cabinet-office.gov.uk>
2026-01-28 12:56:30 +0000

    Show user avatar images when available

--- frontend/src/lib/components/ConversationMessage.svelte
+++ frontend/src/lib/components/ConversationMessage.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
   import type { ConversationMessageProps } from "$lib/types/ConversationMessage"
-  let { message, user, isSelf = true }: ConversationMessageProps = $props();
+  let { message, user, image, isSelf = true }: ConversationMessageProps = $props();
 </script>
 
 <li class="app-c-conversation-message">
   <div class="app-c-conversation-message__message {isSelf ? 'app-c-conversation-message__message--user-message' : 'app-c-conversation-message__message--govuk-message'}">
     <div class="app-c-conversation-message__body {isSelf ? 'app-c-conversation-message__body--user-message' : 'app-c-conversation-message__body--govuk-message'}">
-      {#if user}
-      <div class="app-c-conversation-message__identifier {isSelf ? 'govuk-visually-hidden' : ''}">
-        <strong>{user}</strong>
+      {#if image || user}
+      <div class="app-c-conversation-message__header">
+        {#if image}
+        <img class="app-c-conversation-message__image" src={image} alt="" />
+        {/if}
+        {#if user}
+        <div class="app-c-conversation-message__identifier">
+          <strong>{user}</strong>
+        </div>
+        {/if}
       </div>
       {/if}
       <div class="govuk-body-m govuk-!-margin-bottom-0">{message}</div>

0958fab1c509045b08684a383781a864e0b56b33
swenban <sophie.wenban@digital.cabinet-office.gov.uk>
2026-08-25 18:01:11 +0100

    Provision client WebSocket infrastructure and Lambda integration
    - Instantiate client_websocket_gateway module and ws_client_router Lambda
    
    - Grant IAM permissions for ManageConnections and Bedrock AgentCore runtime invocation

--- infrastructure/services/api_gw_client.tf
+++ infrastructure/services/api_gw_client.tf
@@ -1,3 +1,48 @@
-# TODO: This will instantiate the API Gateway WebSocket module for
-# inbound frontend client connections and internal relaying.
-# It is decoupled from the HTTP CRM Webhook gateway to isolate security profiles.
+# -----------------------------------------------------------------------------
+# WEBSOCKET API GATEWAY
+# -----------------------------------------------------------------------------
+module "client_websocket_gateway" {
+  source            = "../modules/api_gw_websocket"
+  environment       = var.environment
+  api_name          = "client"
+  target_lambda_arn = module.ws_client_router_lambda.invoke_arn
+}
+
+# -----------------------------------------------------------------------------
+# WEBSOCKET ROUTER LAMBDA
+# -----------------------------------------------------------------------------
+module "ws_client_router_lambda" {
+  source        = "../modules/lambda"
+  environment   = var.environment
+  function_name = "ws_client_router"
+  timeout       = 29
+
+  environment_variables = {
+    ENVIRONMENT       = var.environment
+    AGENT_RUNTIME_ARN = aws_bedrockagentcore_agent_runtime.orchestrator_runtime.agent_runtime_arn
+  }
+
+  policy_statements = [
+    {
+      Effect   = "Allow"
+      Action   = ["execute-api:ManageConnections"]
+      Resource = "${module.client_websocket_gateway.execution_arn}/*"
+    },
+    {
+      Effect   = "Allow"
+      Action   = ["bedrock-agentcore:InvokeAgentRuntime"]
+      Resource = "${aws_bedrockagentcore_agent_runtime.orchestrator_runtime.agent_runtime_arn}*"
+    }
+  ]
+}
+
+# -----------------------------------------------------------------------------
+# LAMBDA INVOCATION PERMISSIONS
+# -----------------------------------------------------------------------------
+resource "aws_lambda_permission" "allow_websocket_invoke" {
+  statement_id  = "AllowWebSocketGatewayInvokeClient"
+  action        = "lambda:InvokeFunction"
+  function_name = module.ws_client_router_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${module.client_websocket_gateway.execution_arn}/*/*"
+}
